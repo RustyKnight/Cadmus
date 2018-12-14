892affcf71ab86f83bfc3ca7c11df0d1c328be71
Shane Whitehead <obishiro@gmail.com>
2018-12-14 17:18:44 +1100

    Initial commit

---
+++ Cadmus/Cadmus.h
@@ -0,0 +1,19 @@
+//
+//  Cadmus.h
+//  Cadmus
+//
+//  Created by Shane Whitehead on 14/12/18.
+//  Copyright © 2018 Kaizen Enteripises. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Cadmus.
+FOUNDATION_EXPORT double CadmusVersionNumber;
+
+//! Project version string for Cadmus.
+FOUNDATION_EXPORT const unsigned char CadmusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cadmus/PublicHeader.h>
+
+
---
+++ README.md
@@ -0,0 +1,8 @@
+#  Cadmus
+A logging framework based on a delegation pattern.
+
+The intent is to provide a lightweight, simple, easy to use logging interface, which can then delegate the actual responsibility of the physcial logging to another delegate.
+
+This allows libraries and projects to be developed, indepdently of the logging framework, which can then be implemented or changed at some point in the future without the remaining code become problematic
+
+
