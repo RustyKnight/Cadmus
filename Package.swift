5229b3b2913b2b328a9ec3943f8089edd02ab918
Shane Whitehead <shanew@swann.com>
2019-11-17 12:26:50 +1100

    Add support for SPM

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.1
+//
+//  Package.swift
+//  iOS
+//
+//  Created by Shane Whitehead on 17/11/19.
+//  Copyright © 2019 Kaizen Enteripises. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "Cadmus",
+    platforms: [
+			.macOS(.v10_12), .iOS(.v10), .tvOS(.v11), .watchOS(.v4)
+    ],
+    products: [
+        .library(name: "Cadmus", targets: ["Cadmus"]),
+    ],
+    dependencies: [
+        //.package(url: "https://url/of/another/package/named/Utility", from: "1.0.0"),
+    ],
+    targets: [
+        .target(name: "Cadmus", dependencies: []),
+    ]
+)
