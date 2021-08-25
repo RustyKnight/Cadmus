f2d1d470016ad4e440517e6208c62fc9e3bc066d
Shane Whitehead <obishiro@gmail.com>
2021-08-25 14:41:11 +1000

    Rebuild SPM

--- Package.swift
+++ Package.swift
@@ -1,28 +1,25 @@
-// swift-tools-version:5.1
-//
-//  Package.swift
-//  iOS
-//
-//  Created by Shane Whitehead on 17/11/19.
-//  Copyright © 2019 Kaizen Enteripises. All rights reserved.
-//
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "Cadmus",
-    platforms: [
-			.macOS(.v10_12), .iOS(.v10), .tvOS(.v11), .watchOS(.v4)
-    ],
-    products: [
-        .library(name: "Cadmus", targets: ["Cadmus"]),
-    ],
-    dependencies: [
-        //.package(url: "https://url/of/another/package/named/Utility", from: "1.0.0"),
-    ],
-    targets: [
-			.target(name: "Cadmus",
-							dependencies: [],
-							path: "Sources/Cadmus"),
-    ]
+  name: "Cadmus",
+  products: [
+    // Products define the executables and libraries a package produces, and make them visible to other packages.
+    .library(
+      name: "Cadmus",
+      targets: ["Cadmus"]),
+  ],
+  dependencies: [
+    // Dependencies declare other packages that this package depends on.
+    // .package(url: /* package url */, from: "1.0.0"),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages this package depends on.
+    .target(
+      name: "Cadmus",
+      dependencies: []),
+  ]
 )
